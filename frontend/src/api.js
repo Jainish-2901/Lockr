1ca9e59ee87ae8f02020a881c447348cad9da25e
Jainish Dabgar <jainishdabgar2901@gmail.com>
2025-09-19 15:28:07 +0530

    api updated

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API = axios.create({ baseURL: "http://localhost:5000/api" });
+const API = axios.create({ baseURL: "https://lockr-sigma.vercel.app/api" });
 
 // Add JWT token to requests if logged in
 API.interceptors.request.use((req) => {
