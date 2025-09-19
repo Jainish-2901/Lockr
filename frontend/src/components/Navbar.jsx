b8d053651477422836d456e86c634e3d15a83e93
Jainish Dabgar <jainishdabgar2901@gmail.com>
2025-09-20 00:50:05 +0530

    update navbar

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -73,7 +73,7 @@ export default function Navbar() {
             <Home className="w-5 h-5" /> Home
           </Link>
           <a
-            href="https://github.com/Abdus-8747/Lockr"
+            href="https://github.com/Jainish-2901/Lockr"
             target="_blank"
             rel="noopener noreferrer"
             className="flex items-center gap-1 text-gray-700 hover:text-black"
